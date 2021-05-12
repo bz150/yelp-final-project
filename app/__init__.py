9cf66601b2b7763d2e20d926486bb4b8d7d88e71
bz150 <77802960+bz150@users.noreply.github.com>
2021-05-11 21:48:34 -0400

    Pytest setup (#10) - SETUP PYTEST STRUCTURE
    * created .travis.yml file
    
    integrating travis and beginning steps for pytest
    
    * create structure for pytest
    
    added files and copied example code (unedited)
    
    * commented out code errors
    
    * commented out PIL in requirements.txt
    
    attempting to resolve travis issues
    
    * more comments to resolve travis issues
    
    commented out pytest in travis script
    
    * update pytest requirements
    
    file structure
    
    * uncomment pytest from travis script

---
+++ app/__init__.py
@@ -0,0 +1,27 @@
+
+#
+# this is the app/__init__.py file
+#
+# the presence of this file, even if empty,
+# ... facilitates local imports from one file to another,
+# ... including importing app code for testing purposes
+#
+#
+
+
+def function(x):
+    return x+x
+
+
+
+
+#def function(my_price):
+#    """
+#    Formats a number as currency (USD) with dollar sign, two decimals, and thousands separator
+#    
+#    Params: my_price (numeric, int or float) to be formatted
+#    
+#    Examples: to_usd(21.645)
+#    """
+#    return f"${my_price:,.2f}"
+#
